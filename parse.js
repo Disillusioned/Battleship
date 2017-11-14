9b89a7a63f21a86676655f7d8f38b334f1e937f8
Benny Yamagata <benny.yamagata@gmail.com>
2017-11-13 20:40:27 -0700

    Easy text change with parse function

--- parse.js
+++ parse.js
@@ -16,7 +16,23 @@ function parseJSON(){
     //turn
     //shotsremaining (beta)
 
-    var playerGrid = document.getElementById("playerGrid")
+    var playerBoard = document.getElementById("playerBoard");
+    var compBoard = document.getElementById("compBoard");
+    var compHealth = document.getElementById("compHealth");
+    var playerHealth = document.getElementById("playerHealth");
+    var turn = document.getElementById("turn");
+
+
+    turn.innerHTML = "Turn: " + json.turn;
+    playerHealth.innerHTML = "Your Health: " + json.playerHealth;
+    compHealth.innerHealth = "Computer Health: " + json.compHealth;
+
+    var compCells = compBoard.getElementsByTagName("td");
+    var playerCells = playerBoard.getElementsByTagName("td");
+
+    for(var i = 0; i < sizeof(compCells); ++i){
+        
+    }
 
     
 }
