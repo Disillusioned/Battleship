662710b9cf420ab036926cd893f3bb1a75266154
Benny Yamagata <benny.yamagata@gmail.com>
2017-11-13 22:01:06 -0700

    parse.js file works no bugs, html is fully updated to reflect changes for assignement 6, hit png was changed to add some text to clarify what it was.  Also the EXJson file was updated to fix a bug requiring an extra [0] to be placed before referencing the arrays.

--- parse.js
+++ parse.js
@@ -22,19 +22,52 @@ function parseJSON(){
     var playerHealth = document.getElementById("playerHealth");
     var turn = document.getElementById("turn");
 
+    //Player Board and CompBoard are 2D arrays ex. playerboard.rows[row].cells[col];
 
     turn.innerHTML = "Turn: " + json.turn;
     playerHealth.innerHTML = "Your Health: " + json.playerHealth;
     compHealth.innerHealth = "Computer Health: " + json.compHealth;
 
-    var compCells = compBoard.getElementsByTagName("td");
-    var playerCells = playerBoard.getElementsByTagName("td");
 
-    for(var i = 0; i < sizeof(compCells); ++i){
-        
+    console.info(playerBoard.length);
+
+    var constArraySize = 3;
+    var playerObjGrid = json.playerBoard;
+    var compObjGrid = json.compBoard;
+
+    //Three different values inside of the ObjGrids:
+    // 0 - Means hasnot been targeted, but will be a miss
+    // 1 - Means has been targeted, was a miss
+    // Piece obj - has a parameter if prevTargeted.
+    //      If prevTargeted is 0 - means that has not been targeted
+    //      If prevtargeted is 1 - means has already been shot, was a hit
+
+    //Really only need to display for player board: Pieces, and missed shots against player
+    for(var i = 0; i < constArraySize; ++i){
+        for(var j = 0; j < constArraySize; ++j){
+            if(playerObjGrid[i][j] == 1){ // Is a prevTargeted miss, display water splash
+                playerBoard.rows[i].cells[j].innerHTML = "<img src='miss.png' alt='MISS'>";
+            }
+            else if(playerObjGrid[i][j].prevTargeted == 1){ //is a prevTargeted piece, display fire
+                playerBoard.rows[i].cells[j].innerHTML = "<img src='hit.png' alt='HIT'>";
+            }
+            else if(playerObjGrid[i][j].isPiece == 1){ //is a Piece, but has not been hit, display black box
+                playerBoard.rows[i].cells[j].setAttribute("bgcolor","black");
+            }
+        }
     }
 
-    
+    //For computerBoard, only need to show misses and hits, no pieces, so only show 1's and pieces that prevTargeted = 1
+    for(var i = 0; i < constArraySize; ++i){
+        for(var j = 0; j < constArraySize; ++j){
+            if(compObjGrid[i][j] == 1){ // Is a prevTargeted miss, display water splash
+                compBoard.rows[i].cells[j].innerHTML = "<img src='miss.png' alt='MISS'>";
+            }
+            else if(compObjGrid[i][j].prevTargeted == 1){ //is a prevTargeted piece, display fire
+                compBoard.rows[i].cells[j].innerHTML = "<img src='hit.png' alt='HIT'>";
+            }
+        }
+    }
 }
 
 parseJSON();
---
+++ readme.txt
@@ -0,0 +1,4 @@
+JSON is used on the login page in a small example of how the JSON files can be parsed 
+we use it to populate the small 3x3 grids with different information for hits (fire), misses(splash of water), and pieces
+(so far just black squares).  
+It is also used to display the turn at the last "save" state, and the "health" (amount of hits necessary to win) of both players.  
